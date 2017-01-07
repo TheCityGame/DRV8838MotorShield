b69803a497e16ae0198f1b5cfa8d49b8d1fcdbaf
TheCityGame <mark.hachey@gmail.com>
2017-01-07 15:13:01 -0500

    The header file based on updates
    Tested the header and cpp files on the mega2560 board.

--- DRV8838MotorShield.h
+++ DRV8838MotorShield.h
@@ -1,21 +1,17 @@
-//#ifndef (__AVR_ATmega1280__) || ndef(__AVR_ATmega2560__)
-//#error "This library only supports Arduino Mega boards."
-//#endif
+﻿//This was only written and tested on the Mega2560 board. 
+//use at your own discretion
 #ifndef DRV8838MotorShield_h
 #define DRV8838MotorShield_h
 
 #include <Arduino.h>
 
-class DRV8838MotorShield_h
+class DRV8838MotorShield
 {
   public:  
-    // CONSTRUCTORS
-    DRV8838MotorShield_h();
-	// Default pin selection.
-    DRV8838MotorShield_h(unsigned char DIR1, unsigned char SPEED1, unsigned char SLEEP1, unsigned char DIR2, 
-                           unsigned char SPEED2, unsigned char SLEEP2); 
+    // default constructor. See cpp for pin definitions. 
+    DRV8838MotorShield(); 
 	// User-defined pin selection. 
-    DRV8838MotorShield_h(unsigned char DIR1, unsigned char SPEED1, unsigned char SLEEP1, unsigned char DIR2, 
+    DRV8838MotorShield(unsigned char DIR1, unsigned char SPEED1, unsigned char SLEEP1, unsigned char DIR2, 
                            unsigned char SPEED2, unsigned char SLEEP2);
 	
    
@@ -23,16 +19,22 @@ class DRV8838MotorShield_h
     void init(); 
     void setM1Speed(int speed); 
     void setM2Speed(int speed); 
-    void setSpeeds(int m1Speed, int m2Speed); .
-    
+    void setSpeeds(int m1Speed, int m2Speed); 
+    void setSpeeds(int iSpeed);
+    void sleep() {digitalWrite(mcSleep1Pin, 0); digitalWrite(mcSleep2Pin, 0);}
+    void wake()  {digitalWrite(mcSleep1Pin, 1); digitalWrite(mcSleep2Pin, 1);}
+    void flipM1();
+    void flipM2();
   private:
-    unsigned char mcDir1;
-    unsigned char mcSpeed1;
-    unsigned char mcSleep1;
-    unsigned char mcDir2;
-    unsigned char mcSpeed2;
-    unsigned char mcSleep2;
-    
-    
+    unsigned char mcDir1Pin;
+    unsigned char mcSpeed1Pin;
+    unsigned char mcSleep1Pin;
+    unsigned char mcDir2Pin;
+    unsigned char mcSpeed2Pin;
+    unsigned char mcSleep2Pin;
+    bool mbFlipM1;  //flip the directions if running opposite expectations
+    bool mbFlipM2;
+    bool mbDir1PinState;
+    bool mbDir2PinState;
 };
 #endif

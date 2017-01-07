87cb77fc952efb2a74ac90a8d31fdd395080c7b9
TheCityGame <mark.hachey@gmail.com>
2017-01-07 15:29:25 -0500

    First upload.
    Just a simple example.

--- example.ino
+++ example.ino
@@ -1,1 +1,28 @@
+#include <DRV8838MotorShield.h>
+/*  this example uses the DRV8838MotorShield class. I wanted a simpler class than 
+ *  the one that Pololu wrote and give the user the ability to define the pins in the 
+ *  declaration. I used both SLP pins. There are ways around this if pins are at a premium.
+ *  This is probably overly commented. 
+ */
+
+DRV8838MotorShield motors(7, 8, 9, 10, 11, 12); //set the pins to whatever you want. 
+
+void setup(){
+  motors.init(); //this sets the pins. 
+  //motors.flipM1();  //flip direction. e.g. if wired opposite, or whatever
+  //motors.flipM2();  //uncomment to flip.  
+}
+
+void loop(){
+
+  motors.setM1Speed(120); //spin one wheel forward
+  delay(1000);
+  motors.setM2Speed(120); //spin the other forward
+  delay(1000);
+  motors.setSpeeds(-120, -120); //spin them both backwards. 
+  delay(1000); 
+  motors.sleep();  //put the unit to sleep 
+  delay(1000);
+  //motors.wake(); //wake the motors if you want to keep looping. Or better, just comment the sleep line. 
+}
 
